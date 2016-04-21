b212a86cc359201faf67e342d548855ac9d400b6
Daniel Mann <daniel.mann@incyclesoftware.com>
2016-04-21 10:08:18 -0700

    Updated copyright and moved namespace to within class

--- src/RMWorkflowMigrator.Generator.PowerShell/Model/RollbackActionResolutionResult.cs
+++ src/RMWorkflowMigrator.Generator.PowerShell/Model/RollbackActionResolutionResult.cs
@@ -1,13 +1,13 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ActionParser.cs" company="Microsoft Corporation">
+// <copyright file="RollbackActionResolutionResult.cs" company="Microsoft Corporation">
 //   Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the MIT License (MIT, https://github.com/ALM-Rangers/Migrate-assets-from-RM-server-to-VSO/blob/master/License.txt). This is sample code only, do not use in production environments.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System.Collections.Generic;
-
 namespace Microsoft.ALMRangers.RMWorkflowMigrator.Generator.PowerShell.Model
 {
+    using System.Collections.Generic;
+
     public class RollbackActionResolutionResult
     {
         public IEnumerable<ScriptAction> Actions { get; set; }
